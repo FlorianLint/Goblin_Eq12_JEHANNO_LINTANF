b55303f98ee5bb93227c327e7d653fefc8f4fac8
flori <flori@10.144.195.230>
2025-04-24 10:05:42 +0200

    Database

--- src/database/Database.java
+++ src/database/Database.java
@@ -1,21 +1,42 @@
 package database;
 
-import java.io.File;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.io.FileReader;
+import java.sql.*;
+import java.util.List;
+import com.opencsv.*;
 
 public class Database {
-	public static void main(String[] args) throws Exception {
-		Class.forName( "org.hsqldb.jdbcDriver" );
-		String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
-		String login = "sa";
-		String password = "";
-		try (Connection connection = DriverManager.getConnection( url, login, password )){
-			String requete = "DROP TABLE citation IF EXISTS;";
-			try ( Statement statement = connection.createStatement() ) {
-				statement.executeUpdate( requete );
-			}
-		}
-	}
+	public static void main(String[] args) {
+		try {
+			Connection conn = DriverManager.getConnection("jdbc:hsqldb:file:./database/clientsdb", "SA", "");
+            conn.setAutoCommit(false);
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS Clients (" +
+                    "Nom VARCHAR(100), " +
+                    "Mail VARCHAR(100), " +
+                    "id_site INT)");
+            
+            CSVReader reader = new CSVReaderBuilder(new FileReader("Jeux_de_donnees/petit/init-clients-30-10-Carre.csv"))
+                    .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
+                    .build();
+            List<String[]> lignes = reader.readAll();
+            reader.close();
+            PreparedStatement ps = conn.prepareStatement("INSERT INTO Clients (Nom, Mail, id_site) VALUES (?, ?, ?)");
+
+            for (int i = 1; i < lignes.size(); i++) {
+                String[] ligne = lignes.get(i);
+                ps.setString(1, ligne[0]); // Nom
+                ps.setString(2, ligne[1]); // Mail
+                ps.setInt(3, Integer.parseInt(ligne[3])); // id_site
+                ps.addBatch();
+            }
+            ps.executeBatch();
+            conn.commit();
+            ps.close();
+            conn.close();
+            System.out.println("Import terminé !");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
