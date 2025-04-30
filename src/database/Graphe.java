c59f784e1fba5b108796dcf767809a913752c8f5
Utilisateur <Utilisateur@10.144.195.107>
2025-04-30 12:12:29 +0200

    voila

---
+++ src/database/Graphe.java
@@ -0,0 +1,17 @@
+package database;
+
+import java.util.*;
+import java.io.*;
+
+
+public class Graphe {
+	pr creerle graphe pr faire la pethode dijstra (ce sont les var d'instance)
+	les donnees de l'entrperise permettent de l'initialiser
+	plus courts chemins a enreigstrer ds la data base 
+	
+	on ecrit le code de dijstra ici
+
+}
+
+
+pulp veut la matrice des plus courts chemins
