541695b0627c7c13d7689588d555c0bc3b9a2f5c
flori <flori@10.144.195.230>
2025-05-05 16:16:36 +0200

    test

--- src/entreprise/Entreprise.java
+++ src/entreprise/Entreprise.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class Entreprise {
     public static void main(String[] args) throws Exception {
-        List<Client> clients = CsvUtils.readCsv("Jeux_de_donnees"+File.separator+"petit/init-clients-30-10-Carre.csv", Client.class);
-        List<Entrepot> entrepots = CsvUtils.readCsv("Jeux_de_donnees/petit/init-entrepots-30-5-Carre.csv", Entrepot.class);
-        List<Route> routes = CsvUtils.readCsv("Jeux_de_donnees/petit/init-routes-30-45-Carre.csv", Route.class);
-        List<Site> sites = CsvUtils.readCsv("Jeux_de_donnees/petit/init-sites-30-Carre.csv", Site.class);
+        List<Client> clients = CsvUtils.readCsv("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-clients-30-10-Carre.csv", Client.class);
+        List<Entrepot> entrepots = CsvUtils.readCsv("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-entrepots-30-5-Carre.csv", Entrepot.class);
+        List<Route> routes = CsvUtils.readCsv("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-routes-30-45-Carre.csv", Route.class);
+        List<Site> sites = CsvUtils.readCsv("Jeux_de_donnees"+File.separator+"petit"+File.separator+"init-sites-30-Carre.csv", Site.class);
 
         // Exemple d'affichage
         for (Client c : clients) {
