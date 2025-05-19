70fba6e7d57913114d4a6a553b5f062842b58102
flori <flori@10.144.195.230>
2025-05-19 17:37:44 +0200

    Demande et bordereau

---
+++ src/entreprise/Bordereau.java
@@ -0,0 +1,14 @@
+package entreprise;
+
+import java.util.List;
+
+public class Bordereau {
+	private Entreprise e;
+	List<Client> clients;
+	List<Integer> demandes;
+	List<Entrepot> entrepots;
+	
+	public Bordereau(Entreprise e, String fictxt) {
+		
+	}
+}
---
+++ src/entreprise/Demande.java
@@ -0,0 +1,19 @@
+package entreprise;
+
+public class Demande {
+	private Client client;
+	private int demande;
+	
+	public Client getClient() {
+		return client;
+	}
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public int getDemande() {
+		return demande;
+	}
+	public void setDemande(int demande) {
+		this.demande = demande;
+	}
+}
