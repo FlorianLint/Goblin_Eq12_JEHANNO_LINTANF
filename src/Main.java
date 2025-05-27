79bc2c849ef3c3e6982bcd60d5b194c260369f4d
flori <flori@10.144.195.230>
2025-05-27 18:32:30 +0200

    Affichage du bordereau de livraison dans un fichier .txt

--- src/Main.java
+++ src/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 System.err.println("Le script Python a échoué !");
             }
             
-            // Affiche du résultat
+            // Affiche du résultat dans la console
             Gson gson = new Gson();
             Reader resultReader = new FileReader("Json" + File.separator + "resultat.json");
             JsonResult result = gson.fromJson(resultReader, JsonResult.class);
@@ -47,7 +47,20 @@ public class Main {
                 int entrepotId = result.deliveriesfor_customer.get(i);
                 System.out.println("Le client " + (i+1) + " est livré par l'entrepôt " + entrepotId);
             }
-
+            
+            // Génération du résultat dans un fichier texte
+            FileWriter writer = new FileWriter("Json" + File.separator + "resultat-" + date + ".txt");
+            writer.write("=== Résultat de l'optimisation ===\n");
+            writer.write("Statut : " + result.status + "\n");
+            writer.write("Objectif : " + result.objective + "\n");
+            writer.write("Entrepôts ouverts : " + result.openfacilities + "\n");
+            writer.write("Livraisons :\n");
+            for (int i = 0; i < result.deliveriesfor_customer.size(); i++) {
+                int entrepotId = result.deliveriesfor_customer.get(i);
+                writer.write("Le client " + (i+1) + " est livré par l'entrepôt " + entrepotId + "\n");
+            }
+            writer.close();
+            System.out.println("Résultat écrit dans Json" + File.separator + "resultat-" + date + ".txt");
         } catch (Exception e) {
             e.printStackTrace();
         }
